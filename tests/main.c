6795ecefdac57683699095ced39a763ce07e827c
Maxim Dzabraev <dzabraew@gmail.com>
2017-02-16 21:59:51 +0300

    tests

--- tests/main.c
+++ tests/main.c
@@ -3,6 +3,18 @@
 #include "myclass.h"
 #include "testdir/file.h"
 
+int globalvar = 1;
+
+struct mystruct {
+  int my1;
+  long my2;
+  double my3;
+  struct {
+    int i;
+    char *s;
+  } st;
+};
+
 int f1(int x) {
   return x+1;
 }
@@ -15,6 +27,7 @@ int f3(int x) {
 
 int main(void) {
   int x=1;
+  struct mystruct d;
   const char * longstr = "123456789abcdef123456789abcdef";
   int x1=1,x2=2,x3=3,x4=4,x5=5,x6=6,x7=7,x888888888888888=8888;
   MyClass mycl;
