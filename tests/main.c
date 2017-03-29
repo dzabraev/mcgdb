a7f38e04dc701f086fe0afa07afecf5f5235607f
Maxim Dzabraev <dzabraew@gmail.com>
2017-03-29 21:42:42 +0300

    добавлены переменные с типами данных, на которых были ошибки.

--- tests/main.c
+++ tests/main.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "head.h"
 #include "file.h"
@@ -7,6 +8,10 @@
 
 
 int globalvar = 1;
+
+typedef struct incompl_struct incompl_struct;
+typedef union incompl_union incompl_union;
+
 
 struct mystruct {
   int my1;
@@ -41,6 +46,18 @@ int main(void) {
     const char *s;
   } test_utf8;
   test_utf8.s = "тест кодировки abdc";
+  char charbuf[4]="abc";
+  incompl_struct *is;
+  incompl_union *iu;
+  incompl_union **is2;
+  incompl_union ******is6;
+  is6                   = (incompl_union ******)malloc(sizeof(void *));
+  is6[0]                = (incompl_union *****)malloc(sizeof(void *));
+  is6[0][0]             = (incompl_union ****)malloc(sizeof(void *));
+  is6[0][0][0]          = (incompl_union ***)malloc(sizeof(void *));
+  is6[0][0][0][0]       = (incompl_union **)malloc(sizeof(void *));
+  is6[0][0][0][0][0]    = (incompl_union *)malloc(sizeof(void *));
+  //is6[0][0] = (incompl_union ****)malloc(sizeof(void *));
   int **intarr = (int **)malloc(10*sizeof(int *));
   int ***intarr3 = (int ***)malloc(10*sizeof(int **));
   intarr[0] = (int *)malloc(10*sizeof(int *));
@@ -55,7 +72,7 @@ int main(void) {
   struct mystruct *d = (struct mystruct *) malloc (sizeof(struct mystruct));
   d->my1=5;
   d->my2=1.2;
-  struct mystruct darr[5];
+  struct mystruct darr[2];
   const char * longstr = "123456789abcdef123456789abcdef";
   int x1=1,x2=2,x3=3,x4=4,x5=5,x6=6,x7=7,x888888888888888=8888;
   MyClass mycl;
@@ -64,5 +81,9 @@ int main(void) {
   x=f3(x);
   x=f5(x);
   x=fac(50);
+  printf("%p\n",is);
+  printf("%p\n",iu);
+  printf("%p\n",is2);
+  printf("%p\n",is6);
   return x;
 }
