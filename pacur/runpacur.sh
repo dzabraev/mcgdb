709f2f7cca1f0d100bf58bef14234ac75ad228e4
Maxim Dzabraev <dzabraew@gmail.com>
2017-04-13 19:01:35 +0300

    сборка пакетов, pacur

--- pacur/runpacur.sh
+++ pacur/runpacur.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
-docker run --rm -t -v `pwd`:/pacur pacur/ubuntu-trusty
+#pacur/ubuntu-zesty
+#pacur/fedora-25
+#pacur/archlinux
+#pacur/centos-7
+#pacur/fedora-22
+#pacur/debian-wheezy
+#pacur/debian-jessie
+#pacur/ubuntu-precise
+#pacur/ubuntu-trusty
+#pacur/fedora-24
+#pacur/fedora-23
+#pacur/ubuntu-yakkety
+#pacur/ubuntu-xenial
+#hello-world
+#pacur/fedora-21
+
+#docker run --rm -t -v `pwd`:/pacur pacur/ubuntu-trusty
+docker run --rm -t -v `pwd`:/pacur pacur/fedora-22
