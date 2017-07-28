61ac209f8c3a78d08346c64cd2def3cce3cd81b7
Dzabraev Maxim <mdzabraev@niisi.ras.ru>
2017-07-28 12:56:45 +0400

    копирование настроек терминала

--- tests/iostub.py
+++ tests/iostub.py
@@ -6,24 +6,12 @@ import pty,sys,os,select,socket,fcntl,termios,time,errno
 def do_child(executable,args):
   os.execv(executable,args)
 
-def copy_term_settings(from_fd,to_fd):
-  to_settings = termios.tcgetattr(to_fd)
-  while True:
-    from_settings = termios.tcgetattr(from_fd)
-    if from_settings != to_settings:
-      termios.tcsetattr(to_fd, termios.TCSANOW, from_settings)
-      return
-    time.sleep(0)
-
 def do_parent(port,child_fd):
   sock = socket.socket()
   sock.connect(('localhost',port))
   stdin_fd = sys.stdin.fileno()
   stdout_fd = sys.stdout.fileno()
   sock_fd = sock.fileno()
-  #time.sleep(5)
-  #termios.tcsetattr(stdout_fd, termios.TCSANOW, termios.tcgetattr(child_fd))
-  copy_term_settings(from_fd=child_fd,to_fd=stdout_fd)
   pair={
     stdin_fd : child_fd,
     child_fd : stdout_fd,
@@ -33,6 +21,8 @@ def do_parent(port,child_fd):
   do_nonblock = lambda fd : fcntl.fcntl(fd, fcntl.F_SETFL, fcntl.fcntl(fd, fcntl.F_GETFL) | os.O_NONBLOCK)
   do_nonblock(stdin_fd)
   do_nonblock(child_fd)
+  select.select([child_fd],[],[]) #wait first child output and then change terminal settings
+  termios.tcsetattr(stdout_fd, termios.TCSANOW, termios.tcgetattr(child_fd))
   while True:
     ready_fds = select.select(rlist,[],[])
     ready_read_fds = ready_fds[0]
