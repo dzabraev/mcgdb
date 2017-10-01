a98a81685d013d2b53eb1500d40b5ff0c4e8ffc7
Maxim Dzabraev <dzabraew@gmail.com>
2017-10-02 00:42:04 +0300

    Добавлены инициализации и regexes к variables-ntcnfv

---
+++ tests/testsuite/variables/record.py
@@ -0,0 +1,177 @@
+#coding=utf8
+
+import re
+HEXADDR='0x[0-9a-f]+'
+
+def pattern_addr(pat):
+  import re
+  HEXADDR='0x[0-9a-f]+'
+  return '(?<=%s)%s' % (re.escape(pat),HEXADDR)
+
+def subarray_addr(pat):
+  import re
+  HEXADDR='0x[0-9a-f]+'
+  return '(?<=%s)\*(%s)' % (re.escape(pat),HEXADDR)
+
+REGEXES=[
+    ('aux',pattern_addr('int (*)[3][3][3] arr = ')),
+    ('aux',pattern_addr('incompl_struct * is = ')),
+    ('aux',pattern_addr('incompl_union * iu = ')),
+    ('aux',pattern_addr('int *** intarr3 = ')),
+    ('aux',pattern_addr('const char (*)[4] const_charbuf = ')),
+    ('aux',pattern_addr('int ** intarr = ')),
+    ('aux','{}\s*{}{ADDR}\)'.re.escape('int ** intarr[0:2] = ['),re.escape('int * *(')),
+    ('aux',pattern_addr('incompl_union ** is2 = ')),
+    ('aux',pattern_addr('double * dblarr2 = ')),
+    ('aux',pattern_addr('incompl_union ****** is6 = ')),
+    ('aux',subarray_addr('incompl_union ***** ')),
+    ('aux',subarray_addr('incompl_union **** ')),
+    ('aux',subarray_addr('incompl_union *** ')),
+    ('aux',subarray_addr('incompl_union ** ')),
+    ('aux','{}\s*{ADDR},\s*{ADDR},\s*{ADDR},\s*{ADDR},\s*{ADDR}\s*\]'.format(re.escape('incompl_union ** *(0x613e60)[0:4] = ['),ADDR=HEXADDR)),
+    ('aux','(unsigned )?((long)|(int)|(long long)|(short)) {}'.format(re.escape('(*)[2] tid = '))),
+    ('aux','(unsigned )?((long)|(int)|(long long)|(short)) {}'.format(re.escape('[2] tid'))),
+    ('aux',pattern_addr('mystruct (*)[2] darr = ')),
+    ('aux',pattern_addr('const char *(*)[2] m_char_ptrbuf = ')),
+    ('aux',pattern_addr('unsigned char (*)[4] ucharbuf = ')),
+    ('aux',pattern_addr('mystruct * d = ')),
+    ('aux',pattern_addr('char (*)[4] charbuf = ')),
+    ('aux',pattern_addr('double * dblarr = ')),
+    ('aux',pattern_addr('const char * longstr = ')),
+    ('aux',pattern_addr('void * retval = ')),
+]
+
+
+
+journal=[
+    {
+        "action_num": 1, 
+        "name": "gdb", 
+        "stream": "file testsuite/variables/src/main\nb main\nrun"
+    }, 
+    {
+        "action_num": 4, 
+        "name": "aux", 
+        "stream": "\u001b[<65;22;9M"
+    }, 
+    {
+        "action_num": 5, 
+        "name": "aux", 
+        "stream": "\u001b[<65;22;9M"
+    }, 
+    {
+        "action_num": 6, 
+        "name": "aux", 
+        "stream": "\u001b[<65;22;9M"
+    }, 
+    {
+        "action_num": 7, 
+        "name": "aux", 
+        "stream": "\u001b[<65;22;9M"
+    }, 
+    {
+        "action_num": 8, 
+        "name": "aux", 
+        "stream": "\u001b[<65;22;9M"
+    }, 
+    {
+        "action_num": 9, 
+        "name": "aux", 
+        "stream": "\u001b[<65;22;9M"
+    }, 
+    {
+        "action_num": 10, 
+        "name": "aux", 
+        "stream": "\u001b[<65;22;9M"
+    }, 
+    {
+        "action_num": 11, 
+        "name": "aux", 
+        "stream": "\u001b[<65;22;9M"
+    }, 
+    {
+        "action_num": 12, 
+        "name": "aux", 
+        "stream": "\u001b[<65;22;9M"
+    }, 
+    {
+        "action_num": 13, 
+        "name": "aux", 
+        "stream": "\u001b[<65;22;9M"
+    }, 
+    {
+        "action_num": 14, 
+        "name": "aux", 
+        "stream": "\u001b[<65;22;9M"
+    }, 
+    {
+        "action_num": 15, 
+        "name": "aux", 
+        "stream": "\u001b[<65;22;9M"
+    }, 
+    {
+        "action_num": 16, 
+        "name": "aux", 
+        "stream": "\u001b[<65;22;9M"
+    }, 
+    {
+        "action_num": 17, 
+        "name": "aux", 
+        "stream": "\u001b[<65;22;9M"
+    }, 
+    {
+        "action_num": 18, 
+        "name": "aux", 
+        "stream": "\u001b[<65;22;9M"
+    }, 
+    {
+        "action_num": 19, 
+        "name": "aux", 
+        "stream": "\u001b[<65;22;9M"
+    }, 
+    {
+        "action_num": 20, 
+        "name": "aux", 
+        "stream": "\u001b[<65;22;9M"
+    }, 
+    {
+        "action_num": 21, 
+        "name": "aux", 
+        "stream": "\u001b[<65;22;9M"
+    }, 
+    {
+        "action_num": 22, 
+        "name": "aux", 
+        "stream": "\u001b[<65;22;9M"
+    }, 
+    {
+        "action_num": 23, 
+        "name": "aux", 
+        "stream": "\u001b[<65;22;9M"
+    }, 
+    {
+        "action_num": 24, 
+        "name": "aux", 
+        "stream": "\u001b[<65;22;9M"
+    }, 
+    {
+        "action_num": 25, 
+        "name": "aux", 
+        "stream": "\u001b[<65;22;9M"
+    }, 
+    {
+        "action_num": 26, 
+        "name": "aux", 
+        "stream": "\u001b[<65;22;9M"
+    }, 
+    {
+        "action_num": 27, 
+        "name": "aux", 
+        "stream": "\u001b[<0;54;1M\u001b[<0;54;1m"
+    }, 
+    {
+        "action_num": 28, 
+        "name": "aux", 
+        "stream": "\u001b[<0;47;1M\u001b[<0;47;1m"
+    }
+]
--- tests/testsuite/variables/src/main.cc
+++ tests/testsuite/variables/src/main.cc
@@ -14,9 +14,9 @@ typedef struct incompl_struct incompl_struct;
 typedef union incompl_union incompl_union;
 
 class MyBase {
-  int x;
-  int y;
   public :
+    int x;
+    int y;
     MyBase (int x) {this->x=x; this->y=0;};
     MyBase (int x, int y) {this->x=x; this->y=y;};
     int value(void) {return x;};
@@ -25,10 +25,11 @@ class MyBase {
 };
 
 class MyBase2 {
-  float x;
+  public:
+    float x;
 };
 
-class MyDeriv : MyBase, MyBase2 {
+class MyDeriv : public MyBase, public MyBase2 {
   //double x;
   public:
     MyDeriv(int x) : MyBase(x) {};
@@ -86,8 +87,35 @@ void *fac50(void *arg) {
 
 int main(void) {
   static int arr[3][3][3];
+  for (int i=0;i<3;i++)
+    for (int j=0;j<3;j++)
+      for (int k=0;k<3; k++)
+        arr[i][j][k] = i+10*j+100*k;
+
+  for (int i=0;i<3;i++)
+    for (int j=0;j<3;j++)
+      for (int k=0;k<3; k++)
+        arr[i][j][k] = k+10*i+100*j;
+
+
   MyDeriv mm(1);
+  mm.MyBase::x=11;
+  mm.MyBase::y=12;
+  mm.MyBase2::x=0.5;
+  mm.uni1=13;
+  mm.uni3=14;
+
+  mm.MyBase::x=12;
+  mm.MyBase::y=13;
+  mm.MyBase2::x=0.6;
+  mm.uni1=14;
+  mm.uni3=15;
+
+
+
+
   char charbuf[4]="abc";
+
   {
     int block1=1;
     int block2=2;
@@ -95,26 +123,30 @@ int main(void) {
       int block3=3;
     }
   }
+
   unsigned char ucharbuf[4]="uns";
   const char const_charbuf[4]="def";
-  const char* m_char_ptrbuf[2] = {0,0};
+  const char* m_char_ptrbuf[2] = {(char *)7,(char *)8};
   int x=10;
   m_char_ptrbuf[0] = "123";
   m_char_ptrbuf[1] = "456";
-  incompl_struct *is=0;
-  incompl_union *iu=0;
-  incompl_union **is2=0;
-  incompl_union ******is6=0;
+  incompl_struct *is;
+  incompl_union *iu;
+  incompl_union **is2;
+  incompl_union ******is6;
   int **intarr=0;
   int ***intarr3=0;
   double *dblarr=0;
   double *dblarr2=0;
   void *retval=0;
+
   union {
     int x;
     double y;
   } uni;
-  struct mystruct *d;
+  uni.y=0.9;
+
+
   struct mystruct darr[2];
   const char * longstr = "123456789abcdef123456789abcdef";
   int x1=1,x2=2,x3=3,x4=4,x5=5,x6=6,x7=7,x888888888888888=8888;
@@ -122,23 +154,63 @@ int main(void) {
 
   pthread_t tid[2];
   pthread_create(&tid[0],0,fac50,0);
-  is6                   = (incompl_union ******)malloc(sizeof(void *));
-  is6[0]                = (incompl_union *****)malloc(sizeof(void *));
-  is6[0][0]             = (incompl_union ****)malloc(sizeof(void *));
-  is6[0][0][0]          = (incompl_union ***)malloc(sizeof(void *));
-  is6[0][0][0][0]       = (incompl_union **)malloc(sizeof(void *));
-  is6[0][0][0][0][0]    = (incompl_union *)malloc(sizeof(void *));
+
+  is6                   = (incompl_union ******)malloc(sizeof(void *)*1);
+  is6[0]=0;
+  is6[0]                = (incompl_union *****)malloc(sizeof(void *)*2);
+  is6[0][0]             = (incompl_union ****)malloc(sizeof(void *)*3);
+  is6[0][1]             = (incompl_union ****)malloc(sizeof(void *));
+  is6[0][0][0]          = (incompl_union ***)malloc(sizeof(void *)*4);
+  is6[0][0][1]          = (incompl_union ***)malloc(sizeof(void *)*4);
+  is6[0][0][2]          = (incompl_union ***)malloc(sizeof(void *)*4);
+  is6[0][0][0][0]       = (incompl_union **)malloc(sizeof(void *)*5);
+  is6[0][0][0][1]       = (incompl_union **)malloc(sizeof(void *)*5);
+  is6[0][0][0][2]       = (incompl_union **)malloc(sizeof(void *)*5);
+  is6[0][0][0][3]       = (incompl_union **)malloc(sizeof(void *)*5);
+  is6[0][0][0][0][0]    = (incompl_union *)malloc(sizeof(void *)*6);
+  is6[0][0][0][0][1]    = (incompl_union *)malloc(sizeof(void *)*6);
+  is6[0][0][0][0][2]    = (incompl_union *)malloc(sizeof(void *)*6);
+  is6[0][0][0][0][3]    = (incompl_union *)malloc(sizeof(void *)*6);
+  is6[0][0][0][0][4]    = (incompl_union *)malloc(sizeof(void *)*6);
+
   intarr = (int **)malloc(10*sizeof(int *));
-  intarr3 = (int ***)malloc(10*sizeof(int **));
   intarr[0] = (int *)malloc(10*sizeof(int *));
   intarr[1] = (int *)malloc(10*sizeof(int *));
   intarr[2] = (int *)malloc(10*sizeof(int *));
+  intarr[0][0]=0;
+  intarr[0][1]=1;
+  intarr[0][2]=2;
+  intarr[1][0]=10;
+  intarr[1][1]=11;
+  intarr[1][2]=12;
+  intarr[1][3]=13;
+  intarr[2][0]=20;
+  intarr[2][1]=21;
+  intarr[2][2]=22;
+  intarr[2][3]=23;
+  intarr[2][4]=23;
+
+  intarr3 = (int ***)malloc(10*sizeof(int **));
+  for (int i=0;i<10;i++)
+    intarr3[i]=(int **)i;
+
   dblarr = (double *) malloc(5*sizeof(double));
+  for (int i=0;i<5;i++)
+    dblarr[i]=0.5+i;
   dblarr2 = dblarr;
+
+  struct mystruct *d;
   d = (struct mystruct *) malloc (sizeof(struct mystruct));
   d->my1=5;
-  d->my2=1.2;
+  d->my2=2;
+  d->my3=0.7;
   d->st.arr = (int *)malloc (10*sizeof(int));
+  for(int i=0;i<10;i++)
+    d->st.arr[i] = 2+i;
+  d->st.i=4;
+  d->st.s=0x0;
+  d->st.s3.x=10;
+  d->st.s3.y=20;
 
   pthread_create(&tid[1],0,f1,0);
   x=f2(x);
