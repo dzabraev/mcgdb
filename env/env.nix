ffedc5ea8edb75261550d4c5a1f695dc533a92fc
nightik <nightik@nightik-laptop.(none)>
2017-11-05 13:36:19 +0300

    fix prompt in nix

--- env/env.nix
+++ env/env.nix
@@ -18,5 +18,6 @@ with import <nixpkgs> { };
       pythonPackages.termcolor
       pythonPackages.pexpect
     ];
+    shellHook = '' export PS1='\[\033[1;32m\][nix-shell:\w]$\[\033[0m\] ' '';
   }
 
